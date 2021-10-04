91e9af915bf496300119a722307ba24d36f11f9e
Hugo Lacombe <33695@192.168.114.121>
2021-10-04 15:49:37 +0200

    Version finale de l'exo 1

--- TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
+++ TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
@@ -4,9 +4,11 @@
  */
 package tp1_manipnombresint;
 
+import java.util.Scanner;
+
 /**
- *
- * @author 33695
+ *EXO 1
+ * @author HugoLACOMBE;BaptisteBArberan
  */
 public class TP1_manipNombresInt {
 
@@ -15,6 +17,20 @@ public class TP1_manipNombresInt {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+int entier1 =0 ;
+int entier2= 0 ;
+Scanner sc=new Scanner(System.in);
+// Scanner=> module pour récupérer les données uti + créé un espace de stockage 
+System.out.println("Rentrez deux entiers");
+entier1= sc.nextInt(); 
+System.out.println("Deuxième entier");
+entier2= sc.nextInt();
+//Saissie des deux nombres
+System.out.println(" La somme est "+(entier1+entier2));
+System.out.println(" La différence du premier entier et du deuxième est "+(entier1-entier2));
+System.out.println(" La multiplication des deux nombres est "+(entier1*entier2));
+System.out.println(" La division des deux chiffres est "+(entier1/entier2));
+System.out.println(" Le reste de la division euclidienne de l'entier 1 par l'entier 2  est "+(entier1%entier2));
     }
-    // NIQUEMOK
-}
+
+    }   
