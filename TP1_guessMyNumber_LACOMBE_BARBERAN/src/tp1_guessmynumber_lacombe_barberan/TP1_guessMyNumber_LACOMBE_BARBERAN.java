82c94852e00ada6deb7f06ab5f5616d2805ebdb8
Hugo Lacombe <33695@192.168.114.121>
2021-10-04 16:30:26 +0200

    Initialisation exo 3 tp 1

--- TP1_guessMyNumber_LACOMBE_BARBERAN/src/tp1_guessmynumber_lacombe_barberan/TP1_guessMyNumber_LACOMBE_BARBERAN.java
+++ TP1_guessMyNumber_LACOMBE_BARBERAN/src/tp1_guessmynumber_lacombe_barberan/TP1_guessMyNumber_LACOMBE_BARBERAN.java
@@ -4,6 +4,9 @@
  */
 package tp1_guessmynumber_lacombe_barberan;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *EXERCICE 3
  * @author Hugo LACOMBE/Baptiste BArberan 
@@ -15,6 +18,23 @@ public class TP1_guessMyNumber_LACOMBE_BARBERAN {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        Random generateurAleat = new Random();
+    int n = generateurAleat.nextInt(100);
+    System.out.println(n);
+    n = generateurAleat.nextInt(100);
+    System.out.println(n);
+    n = generateurAleat.nextInt(100);
+    System.out.println(n);
+    n = generateurAleat.nextInt(100);
+    System.out.println(n);
+    n = generateurAleat.nextInt(100);
+    System.out.println(n);
+    //Affichage des 5 nombres aléatoires générées grace au module Random generator
+    // Premiere étape générer un nombre aléatoire entre 0 et 100 => nbadev
+    int nbadev = generateurAleat.nextInt(); 
+    //On demande à l'utilisateur de saissir un nombre entre 0 et 100 
+    Scanner sc = new Scanner(System.in); 
     
-}
+    
+    }
+   
