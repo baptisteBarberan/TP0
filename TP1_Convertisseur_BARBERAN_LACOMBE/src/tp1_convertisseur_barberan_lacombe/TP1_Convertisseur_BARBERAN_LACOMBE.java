4d9fd180c0263ce5b53752ff757e8c7cd5dbcfaa
Baptiste <Baptiste@192.168.114.167>
2021-10-04 17:08:52 +0200

    Modification projet

--- TP1_Convertisseur_BARBERAN_LACOMBE/src/tp1_convertisseur_barberan_lacombe/TP1_Convertisseur_BARBERAN_LACOMBE.java
+++ TP1_Convertisseur_BARBERAN_LACOMBE/src/tp1_convertisseur_barberan_lacombe/TP1_Convertisseur_BARBERAN_LACOMBE.java
@@ -21,11 +21,38 @@ public class TP1_Convertisseur_BARBERAN_LACOMBE {
     System.out.println("Rentrez votre temperature en celcius");
     valeur= sc.nextDouble(); //valeur est demandé à l'utilisateur est la temp en celcius
     System.out.println("la temperature converti en kelvin est "+ (valeur + 273.5));
-   
-    //deuxieme methode pour convertir la temp avec la fonction 
+    
+    //Formulaire :
+    
+    int numero=0;
+    while(numero>6 && numero<0){
+      System.out.println("Rentrez un numéro entre 1 et 6 faites votre choix");    
+      numero= sc.nextInt(); //valeur est demandé à l'utilisateur
+    }
+    switch (numero){
+            case 1:// celcius ver Kelvin 
     double tempC=sc.nextDouble();
     double tempK=CelciusVersKelvin(tempC);
     System.out.println("la temperature converti en kelvin est "+tempK);
+            
+            case 2://convertir Kelvin vers Celcius  
+    double tempK1=sc.nextDouble();
+    double tempC1=KelvinVersCelcius(tempK1);
+    System.out.println("la temperature converti en kelvin est "+tempC1);
+    
+            case 3://convertir Farenheit vers Celcius  
+                
+    double tempF1=sc.nextDouble();
+    double tempC2=KelvinVersCelcius(tempF1);
+    System.out.println("la temperature converti en kelvin est "+tempC2);
+    
+            case 4://convertir 
+                    
+    
+    
+    
+    
+    
     }
     
     
@@ -46,5 +73,11 @@ public static double FarenheitVersCelcius(double tempFarenheit	){
 public static double CelciusVersFarenheit(double tempCelcius){
 double a= (tempCelcius*1.8)+32;
     return (a);   
+}
+public static double KelvinVersFarenheit(double tempKelvin){
+    double a=KelvinVersCelcius(tempKelvin);   
+     a=CelciusVersFarenheit(a);
+     return a ;
+    
 }
 }
