d954463ceb85b04be2a404652d9e1af471357e88
Hugo Lacombe <33695@192.168.114.121>
2021-10-07 22:15:40 +0200

    mode simple / facile fait

--- TP1_guessMyNumber_LACOMBE_BARBERAN/src/tp1_guessmynumber_lacombe_barberan/TP1_guessMyNumber_LACOMBE_BARBERAN.java
+++ TP1_guessMyNumber_LACOMBE_BARBERAN/src/tp1_guessmynumber_lacombe_barberan/TP1_guessMyNumber_LACOMBE_BARBERAN.java
@@ -35,31 +35,33 @@ public class TP1_guessMyNumber_LACOMBE_BARBERAN {
         // Mode facile => classique nb dee coups illimité indication plus simple 
         // Mode normal=> nombre de coup illimité grand nombre
         // Mode difficile => indication peu detaillée, nombre de coup limité, intervalle plus grand 
-        
+        //Mode cauchemar => ajout des fausses indications 
         //Debut du jeu 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Choissisez votre mode de difficulté");
-        System.out.println("1) Mode facile\n2) Mode simple\n3) Mode difficile");
+        System.out.println("1) Mode facile\n2) Mode simple\n3) Mode difficile\n4) Mode Cauchemar");
         int mode;
-        //Saisie du mode par l'utilisateur
+        
         mode = sc.nextInt();
-        // choix du mode par l'utilisateur 
-        boolean rep = false;
-        int nbutil;
-        int nbadev;
-        int tent = 0;
-        if (mode == 1) { // mode facile 
+       
+        
+        int nbutil;// nombre de l'utilisateur
+        int nbadev;// nombre à deviner
+        int tent = 0;//nombre de tentative
+        
+        // Mode facile 
+        if (mode == 1) { 
             //On demande à l'utilisateur de saissir un nombre entre 0 et 100 
             // On va générer un nombre aléatoire entre 0 et 100 => nbadev
             nbadev = generateurAleat.nextInt(100);
-            System.out.println(nbadev); // A ENLEVER
+            
 
-            // initialisation du nombre saissi par l'utilisateur 
+
             // boucle interraction utilisateur 
             while (true) {
                 while (true) {
-                    System.out.println("Saissisez un nombre entier entre 0 et 100");
+                    System.out.println("Saissis un nombre entier entre 0 et 100");
                     nbutil = sc.nextInt();
                     if (nbutil >= 0 && nbutil <= 100) {
                         break;
@@ -68,7 +70,7 @@ public class TP1_guessMyNumber_LACOMBE_BARBERAN {
                 tent += 1;// une tentative est ajoutée
 
                 if (nbutil == nbadev) {
-                    rep = true;
+                    
                     System.out.print("Gagné!!");
                     break;
                 }
@@ -98,7 +100,7 @@ public class TP1_guessMyNumber_LACOMBE_BARBERAN {
 
                 }
                 if (nbutil > nbadev) {
-                    int D1 = (nbutil - nbadev);
+                    int D1 = (nbutil - nbadev);// différence pour connaître la position du nombre de l'utilisateur par rapport au nombre à deviner 
                     if (D1 >= 20) {
                         System.out.println("Tu es encore trop loin, c'est plus petit !");
                         continue;
@@ -129,7 +131,7 @@ public class TP1_guessMyNumber_LACOMBE_BARBERAN {
             nbadev = generateurAleat.nextInt(100);
             while (true) {
                 while (true) {
-                    System.out.println("Saissisez un nombre entier entre 0 et 100");
+                    System.out.println("Saissis un nombre entier entre 0 et 100");
                     nbutil = sc.nextInt();
                     if (nbutil >= 0 && nbutil <= 100) {
                         break;
@@ -138,7 +140,7 @@ public class TP1_guessMyNumber_LACOMBE_BARBERAN {
                 tent += 1;// une tentative est ajoutée
 
                 if (nbutil == nbadev) {
-                    rep = true;
+                    
                     System.out.print("Gagné!!");
                     break;
                 }
@@ -152,8 +154,97 @@ public class TP1_guessMyNumber_LACOMBE_BARBERAN {
                 }
 
             }
-
         }
+            //Mode Difficile
+            if (mode == 3) {
+                System.out.println("Tu as choissis le mode difficile, sois stratégique les tentatives sont limitées (10 coups) ");
+                nbadev = generateurAleat.nextInt(100);
+                while (true) {
+                    while (true) {
+                        System.out.println("Saissis un nombre entier entre 0 et 100");
+                        nbutil = sc.nextInt();
+                        if (nbutil >= 0 && nbutil <= 100) {
+                            break;
+                        }
+                    }
+                    tent += 1;// une tentative est ajoutée
+
+                    if (nbutil == nbadev) {
+                        
+                        System.out.print("Gagné!!");
+                        break;
+                    }
+
+                    if (nbutil < nbadev) {
+                        System.out.println("Plus grand");
+                    }
+                    if (nbutil > nbadev) {
+                        System.out.println("Plus petit");
+
+                    }
+                    if (tent == 10) {
+                        System.out.println("Tu as utilisé des dix tentatives, c'est fini tu as perdu !");
+                        break;
+                    }
+
+                }
+
+            }
+
+            //Mode cauchemar
+            int temp;
+            if (mode == 4) {
+                System.out.println("Tu as choissis le mode cauchemar, sois stratégique les tentatives sont limitées (10 coups), parfois les indications seront fausses ");
+                nbadev = generateurAleat.nextInt(100);
+                
+                while (true) {
+                    while (true) {
+                        System.out.println("Saissis un nombre entier entre 0 et 100");
+                        nbutil = sc.nextInt();
+                        if (nbutil >= 0 && nbutil <= 100) {
+                            break;
+                        }
+                    }
+                    tent += 1;// une tentative est ajoutée
+
+                    if (nbutil == nbadev) {
+                        
+                        System.out.print("Gagné!!");
+                        break;
+                    }
+
+                    if (nbutil < nbadev) {
+                        temp = generateurAleat.nextInt(9);//A enlever
+                        
+                        if (temp == 0 || temp == 1 || temp == 2) {
+                            System.out.println("Plus petit");
+                            continue;
+
+                        }
+                        System.out.println("Plus grand");
+                    }
+                    if (nbutil > nbadev) {
+                        temp = generateurAleat.nextInt(9);
+                        
+                        if (temp == 0 || temp == 1 || temp == 2) {
+                            System.out.println("Plus grand");
+                            continue;
+                        }
+                        System.out.println("Plus petit");
+
+                    }
+                    if (tent == 10) {
+                        System.out.println("Tu as utilisé des dix tentatives, c'est fini tu as perdu !");
+                        break;
+                    }
+
+                }
+
+            }
+
+            System.out.println(" Nombre de tentative : " + tent);
+
+        
 
         System.out.println(" Nombre de tentative : " + tent);
 
