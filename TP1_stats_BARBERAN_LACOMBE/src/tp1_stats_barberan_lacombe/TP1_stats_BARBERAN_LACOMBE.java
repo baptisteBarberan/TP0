9f99c1106939d59880f63738873d19b52334283d
Baptiste <Baptiste@192.168.114.167>
2021-10-07 21:17:54 +0200

    presque fini bloque question 3 a reprendre la boucle et taille de tab1

--- TP1_stats_BARBERAN_LACOMBE/src/tp1_stats_barberan_lacombe/TP1_stats_BARBERAN_LACOMBE.java
+++ TP1_stats_BARBERAN_LACOMBE/src/tp1_stats_barberan_lacombe/TP1_stats_BARBERAN_LACOMBE.java
@@ -4,9 +4,13 @@
  */
 package tp1_stats_barberan_lacombe;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * TP1_stats
- * @author BARBERAN LACOMBE 
+ *
+ * @author BARBERAN LACOMBE
  */
 public class TP1_stats_BARBERAN_LACOMBE {
 
@@ -14,7 +18,25 @@ public class TP1_stats_BARBERAN_LACOMBE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        int[] tab1; //initialisation tableau nommé tab1 
+        tab1 = new int[6];  //Initialisation de la taile du tableau a 6
+
+        for (int cpt = 0; cpt <= 5; cpt++) { // boucle qui parcourt le tableau et le rempli de 0
+            tab1[cpt] = 0;
+            // attention la premiere valeur de tab1 commence a l'indice 0 
+        }
+
+        Scanner sc = new Scanner(System.in); // création de l'espace de stockage 
+
+        System.out.println("Rentrez un nombre entier ");  //affichage 
+        int m = sc.nextInt();  // demande a l'utilisateur du nombre entier 
+
+        Random generateurAleat = new Random();//importation du module aleatoire 
+        int nbaleat = generateurAleat.nextInt(100);
         
+        for (int cpt = 0; cpt <= m; cpt++) { // boucle qui parcourt le tableau et le rempli de 0
+            tab1[cpt] = 0;// probleme entre taille de tableau et nombre itération je pense que le tableau doit faire la taille m
+        }
     }
-    
 }
