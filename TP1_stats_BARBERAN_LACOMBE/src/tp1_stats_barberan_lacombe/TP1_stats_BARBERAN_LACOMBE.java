815c6266d5244da63259e8c19e929c15e2131f3a
Baptiste <Baptiste@192.168.114.167>
2021-10-07 20:36:53 +0200

    Début exrcice 4 TP1

---
+++ TP1_stats_BARBERAN_LACOMBE/src/tp1_stats_barberan_lacombe/TP1_stats_BARBERAN_LACOMBE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_barberan_lacombe;
+
+/**
+ * TP1_stats
+ * @author BARBERAN LACOMBE 
+ */
+public class TP1_stats_BARBERAN_LACOMBE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
