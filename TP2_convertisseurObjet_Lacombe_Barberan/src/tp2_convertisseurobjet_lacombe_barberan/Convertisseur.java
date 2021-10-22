4d6200da7c9d59c4ead3895787e65d321e7dd67b
Baptiste <Baptiste@192.168.114.167>
2021-10-22 14:58:33 +0200

    Création et debut exercice 2 Convertisseur

---
+++ TP2_convertisseurObjet_Lacombe_Barberan/src/tp2_convertisseurobjet_lacombe_barberan/Convertisseur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_lacombe_barberan;
+
+/**
+ *
+ * @author Baptiste
+ */
+public class Convertisseur {
+    int nbConversions=0;
+    
+
+@Override
+
+public String tosString () {
+ return "nb de conversions"+ nbConversions;
+}
+}
