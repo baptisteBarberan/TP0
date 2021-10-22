806333315d1133e2a9c1b23b89f5b558664c0c19
Hugo Lacombe <33695@192.168.114.121>
2021-10-22 14:32:42 +0200

    debut exo1 tp2

---
+++ TP2_Bieres_Lacombe_Barberan/src/tp2_bieres_lacombe_barberan/BouteilleBiere.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_lacombe_barberan;
+
+/**
+ *
+ * @author 33695
+ */
+public class BouteilleBiere {
+    String nom; 
+    float  degreAlcool;
+    String  brasserie;
+    boolean ouverte; 
+   
+    public void lireEtiquette() {
+    System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+            " degres) \nBrasserie : " + brasserie ) ;
+    }
+
+}
+
+ 
