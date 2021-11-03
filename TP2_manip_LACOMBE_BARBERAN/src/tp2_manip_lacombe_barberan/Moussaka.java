1be23d7732a14c6526c6e95beed18b810011cdf2
Hugo Lacombe <33695@192.168.114.121>
2021-11-03 22:33:46 +0100

    Tp2 exo 1 terminé, Hugo Lacombe

---
+++ TP2_manip_LACOMBE_BARBERAN/src/tp2_manip_lacombe_barberan/Moussaka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_lacombe_barberan;
+
+/**
+ *
+ * @author Hugo.l
+ */
+public class Moussaka {
+    int nbCalories;
+    public Moussaka(int Calories){
+        nbCalories=Calories;
+    }
+}
---
+++ TP2_manip_LACOMBE_BARBERAN/src/tp2_manip_lacombe_barberan/Tartiflette.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_lacombe_barberan;
+
+/**
+ *
+ * @author Hugo.L
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette(int Calories){
+        nbCalories=Calories;
+    }
+}
