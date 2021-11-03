d65c754de65c9710b67b01709d51b8a9e0a111f9
Hugo Lacombe <33695@192.168.114.121>
2021-11-03 22:12:17 +0100

    Tp2 exo 1 terminé, Hugo Lacombe

--- TP2_Bieres_Lacombe_Barberan/src/tp2_bieres_lacombe_barberan/BouteilleBiere.java
+++ TP2_Bieres_Lacombe_Barberan/src/tp2_bieres_lacombe_barberan/BouteilleBiere.java
@@ -6,7 +6,7 @@ package tp2_bieres_lacombe_barberan;
 
 /**
  *
- * @author 33695
+ * @author Hugo Lacombe
  */
 public class BouteilleBiere {
     String nom; 
@@ -25,10 +25,14 @@ uneBrasserie) {
  brasserie = uneBrasserie;
  ouverte = false;
 }
+/*/
+Définition du nouveau constructeur et on écrase celui déjà existant
+/*/
 
 public boolean Decapsuler(){
     if(ouverte==false){
         ouverte=true;
+         return true;
         
       
     }
@@ -36,6 +40,22 @@ public boolean Decapsuler(){
         System.out.println("Erreur la bière était déjà ouverte");
         return false;
     }
+}
+/*/
+méthode Décapsuler permet de modifier l'attribut ouverte d'un objet de cette classe
+/*/
+@Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+/*/
+La méthode toSting() est une méthode qui est appelé avec la méthode System.out.println()
+Cette méthode existe de base, la commande @Override permet d'écraser la méthode déjà existante
+
+/*/
 }
 }
 
--- TP2_Bieres_Lacombe_Barberan/src/tp2_bieres_lacombe_barberan/TP2_Bieres_Lacombe_Barberan.java
+++ TP2_Bieres_Lacombe_Barberan/src/tp2_bieres_lacombe_barberan/TP2_Bieres_Lacombe_Barberan.java
@@ -20,19 +20,31 @@ public class TP2_Bieres_Lacombe_Barberan {
 /*/Lorsque nous utilisons le constructeur par défaut => BouteilleBiere uneBiere = new BouteilleBiere()
     On n'affecte pas les valeurs des différents attributs directement lors de l'initialisaton de notre variable qui appartient à 
         la classe BouteilleBiere quand c'est par le constructeur est celui par défault . 
-        Ici on en a crée un nouv constructeur qui nous permet d'affecter tous les attributs de la classe
+        Ici on en a crée un nouveau constructeur qui nous permet d'affecter tous les attributs de la classe
         
 */
         uneBiere.lireEtiquette();
+        
         BouteilleBiere secondeBiere = new BouteilleBiere("Leffe",
 6.6,"Abbaye de Leffe") ;
           secondeBiere.lireEtiquette();
           
         BouteilleBiere troisBiere = new BouteilleBiere("Heinekein",8.0,"LA brasserie de jul");
         troisBiere.lireEtiquette();
-        troisBiere.Decapsuler();
-        troisBiere.Decapsuler();
         
+        //On crée des objets de la classe bouteilleBiere
+        
+        System.out.println(troisBiere.Decapsuler());
+        troisBiere.Decapsuler();
+        System.out.println(troisBiere) ;
+        /*/la ligne d'au dessus permet d'afficher la référence de l'objet.
+        En définissant la méthode toString on peut afficher ce l'on souhaite avec 
+cette ligne.
+/*/
+
+        BouteilleBiere quatBiere = new BouteilleBiere("Corona",14.0,"Chez tonton");
+        quatBiere.Decapsuler();
+        System.out.println(quatBiere);
     }
     
 }
