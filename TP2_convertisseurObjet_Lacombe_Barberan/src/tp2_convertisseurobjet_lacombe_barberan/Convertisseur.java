9b7d3a788e2b9c65c3aa8f901ccc4fe34278d7b0
Baptiste <Baptiste@192.168.114.167>
2021-11-03 23:34:10 +0100

    Classe Convertisseur crée et fini dans Ex2 debut utilisation des methodes

--- TP2_convertisseurObjet_Lacombe_Barberan/src/tp2_convertisseurobjet_lacombe_barberan/Convertisseur.java
+++ TP2_convertisseurObjet_Lacombe_Barberan/src/tp2_convertisseurobjet_lacombe_barberan/Convertisseur.java
@@ -9,12 +9,51 @@ package tp2_convertisseurobjet_lacombe_barberan;
  * @author Baptiste
  */
 public class Convertisseur {
-    int nbConversions=0;
+
+int nbConversions    ; // création modele 
+     
     
+public  Convertisseur () { //methode convertisseur 
+ nbConversions = 0 ;
+}
 
 @Override
-
-public String tosString () {
+public String toString () {// methode affichage 
  return "nb de conversions"+ nbConversions;
 }
+
+    public double CelciusVersKelvin(double tempCelcius) {
+        double a = (tempCelcius + 273.5);
+        return (a);
+    }
+    public double KelvinVersCelcius(double tempKelvin) {
+        double a = (tempKelvin - 273.5);
+        return (a);
+    }
+
+    public  double FarenheitVersCelcius(double tempFarenheit) {
+        double a = (tempFarenheit - 32.0) / 1.8;
+        return (a);
+    }
+
+    public double CelciusVersFarenheit(double tempCelcius) {
+        double a = (tempCelcius * 1.8) + 32;
+        return (a);
+    }
+
+    public double KelvinVersFarenheit(double tempKelvin) {
+        double a = KelvinVersCelcius(tempKelvin);
+        a = CelciusVersFarenheit(a);
+        return a;
+
+    }
+
+    public double FarenheitVersKelvin(double tempFarenheit) {
+        double a = FarenheitVersCelcius(tempFarenheit);
+        a = CelciusVersKelvin(a);
+        return a;
+    }
+
+
 }
+
