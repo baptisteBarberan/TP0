a38f68609d7c17d7557bcf73ba079f60203e0481
Baptiste <Baptiste@192.168.114.167>
2021-11-10 23:49:03 +0100

    Exercice 4 terminé tout fonctionnel !

--- TP2_relation_1_BARBERAN_LACOMBE/src/tp2_relation_1_barberan_lacombe/TP2_relation_1_BARBERAN_LACOMBE.java
+++ TP2_relation_1_BARBERAN_LACOMBE/src/tp2_relation_1_barberan_lacombe/TP2_relation_1_BARBERAN_LACOMBE.java
@@ -19,18 +19,33 @@ Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
 Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
 Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
 Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+Voiture uneFefe = new Voiture ("Ferrari", "SF 90", 5 ) ;
+Voiture une911 = new Voiture ("Porsche","911 turbo S", 20);
 Personne bob = new Personne("Bobby", "Sixkiller");
 Personne reno = new Personne("Reno", "Raines");
 
 System.out.println("liste des voitures disponibles "+ uneClio +
-"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra + "\n" + uneFefe) ;
 
 bob.liste_voitures[0] = uneClio ;
-bob.nbVoitures = 1 ;
+bob.liste_voitures[1]= une2008;
+bob.nbVoitures = 2 ;
+une2008.proprietaire= bob ;
 uneClio.proprietaire = bob ;
 
-System.out.println("la premiere voiture de Bob est " +
-bob.liste_voitures[0] ) ;
+reno.liste_voitures[0]= uneFefe;
+reno.liste_voitures[1]=uneMicra;
+uneFefe.proprietaire=reno;
+uneMicra.proprietaire=reno;
+reno.nbVoitures=2;
+        
+System.out.println("la premiere voiture de reno est\n " +
+reno.liste_voitures[0] + "et la deuxieme est\n"+ reno.liste_voitures[1]+"\nLe nombre de voiture de bob est "+bob.nbVoitures+" et de reno est "+ reno.nbVoitures );
 
+
+
+bob.ajouter_voiture(uneFefe);
+bob.ajouter_voiture(uneAutreClio);
+bob.ajouter_voiture(une911);
 }
 }
