bb304b45d746a2eec418d7446e5f46fde792b1f7
Hugo Lacombe <33695@192.168.114.121>
2021-11-12 19:05:42 +0100

    classe grille commencement

--- SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Grille.java
+++ SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Grille.java
@@ -9,6 +9,28 @@ package sp4_console_lacombe_barberan;
  * @author Hugo Lacombe
  */
 public class Grille {
-  //  Cellule [6][7]CellulesJeu ;
+  Cellule [][] CellulesJeu= new Cellule [6][7] ;
+  
+  public Grille(){
+      for (int i=0;i<=6;i++){
+         
+          for(int j=0;j<=7;j++){
+              
+          
+          CellulesJeu[i][j]= new Cellule();
+      }
+      }
+  }
+ public boolean ajouterJetonDansColonne(Jeton jeton, int colonne){
+     for (int i=5;i>=0;i--){
+          if(CellulesJeu[i][colonne].jetonCourant==null){
     
+     CellulesJeu[i][colonne].jetonCourant=jeton;
+     return true ;
+     
+ }
+          
+}
+     return false ;
+ }
 }
