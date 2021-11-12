2755e3c969b52b36c7b5203bbe01ba7b87bbd36b
Hugo Lacombe <33695@192.168.114.121>
2021-11-12 16:41:24 +0100

    création

---
+++ SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/SP4_console_LACOMBE_BARBERAN.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_lacombe_barberan;
+
+/**
+ *
+ * @author 33695
+ */
+public class SP4_console_LACOMBE_BARBERAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
