65442abba57faf45cca06793d425536229ddabef
Hugo Lacombe <33695@192.168.114.121>
2021-11-12 16:50:03 +0100

    création

---
+++ SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Jeton.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_lacombe_barberan;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Jeton {
+    String Couleur;
+    
+    public Jeton(String couleur){
+        couleur=Couleur; 
+    
+    }
+    public String lireCouleur(){
+        return Couleur;
+    }
+    
+}
---
+++ SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Joueur.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_lacombe_barberan;
+
+/**
+ *
+ * @author Hugo.L
+ */
+public class Joueur {
+    String Nom;
+    String Couleur;
+    J
+}
