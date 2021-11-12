86e73e867b0651dfddecbabb4746f684a7c45a9e
Baptiste <Baptiste@192.168.114.167>
2021-11-12 18:32:30 +0100

    Classe cellule crée jusqu'a placerTrouNoir()

--- SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Cellule.java
+++ SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Cellule.java
@@ -14,10 +14,46 @@ boolean trouNoir;
 boolean desintegrateur;
 
 public Cellule(){
-jetonCourant;
+jetonCourant=null;
 trouNoir=false ;
 desintegrateur=false;}
 
-public affecterJeton(Jeton){
-//if(Cellule) =;
+public boolean affecterJeton(Jeton Jeton){ // affecte le jeton en paramètre ou retourne faux si cellule pleine 
+if(jetonCourant==null){
+System.out.print("Cellule occupé ");
+    return false ;
+}
+else{
+    jetonCourant= Jeton;
+       return true ; }}
+
+
+
+
+public String lireCouleurDuJeton(){
+if (jetonCourant==null){
+    return "vide";
+}
+    else {
+            return jetonCourant.lireCouleur();
+            }    
+}
+
+public Jeton recupererJeton(){
+return jetonCourant;
+}
+
+public boolean supprimerJeton() {
+ if(jetonCourant==null){
+     System.out.print("Jeton innexistant ");
+return false ;
+}
+ else{ jetonCourant=null;
+    return true ;
+     }
+}
+
+//public boolean placerTrouNoir(){
+  //  trouNoir.placerTrouNoir(int, int);
+//}
 }
