abe3cd5f1305e6393b29dfe9bf1c6961376b1295
Baptiste <Baptiste@192.168.114.167>
2021-11-12 17:24:14 +0100

    Création classe Cellule

---
+++ SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Cellule.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_lacombe_barberan;
+
+/**
+ *
+ * @author Baptiste
+ */
+public class Cellule {
+Jeton jetonCourant;
+boolean trouNoir;
+boolean desintegrateur;
+
+public Cellule(){
+jetonCourant;
+trouNoir=false ;
+desintegrateur=false;}
+
+public affecterJeton(Jeton){
+//if(Cellule) =;
+}
