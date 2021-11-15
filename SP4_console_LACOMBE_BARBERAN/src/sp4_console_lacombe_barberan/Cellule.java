f52b68fd7fa21f4d3cfb4b3e8eb8956f74f1a259
Baptiste <Baptiste@192.168.114.167>
2021-11-15 10:47:37 +0100

    Classe cellule crée jusqu'a placerTrouNoir()

--- SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Cellule.java
+++ SP4_console_LACOMBE_BARBERAN/src/sp4_console_lacombe_barberan/Cellule.java
@@ -9,51 +9,91 @@ package sp4_console_lacombe_barberan;
  * @author Baptiste
  */
 public class Cellule {
-Jeton jetonCourant;
-boolean trouNoir;
-boolean desintegrateur;
 
-public Cellule(){
-jetonCourant=null;
-trouNoir=false ;
-desintegrateur=false;}
+    Jeton jetonCourant;
+    boolean trouNoir;
+    boolean desintegrateur;
 
-public boolean affecterJeton(Jeton Jeton){ // affecte le jeton en paramètre ou retourne faux si cellule pleine 
-if(jetonCourant==null){
-System.out.print("Cellule occupé ");
-    return false ;
-}
-else{
-    jetonCourant= Jeton;
-       return true ; }}
+    public Cellule() {
+        jetonCourant = null;
+        trouNoir = false;
+        desintegrateur = false;
+    }
 
+    public boolean affecterJeton(Jeton Jeton) { // affecte le jeton en paramètre ou retourne faux si cellule pleine 
+        if (jetonCourant == null) {
+            System.out.print("Cellule occupé ");
+            return false;
+        } else {
+            jetonCourant = Jeton;
+            return true;
+        }
+    }
 
-
-
-public String lireCouleurDuJeton(){
-if (jetonCourant==null){
-    return "vide";
-}
-    else {
+    public String lireCouleurDuJeton() {
+        if (jetonCourant == null) {
+            return "vide";
+        } else {
             return jetonCourant.lireCouleur();
-            }    
-}
+        }
+    }
 
-public Jeton recupererJeton(){
-return jetonCourant;
-}
+    public Jeton recupererJeton() {
+        return jetonCourant;
+    }
 
-public boolean supprimerJeton() {
- if(jetonCourant==null){
-     System.out.print("Jeton innexistant ");
-return false ;
-}
- else{ jetonCourant=null;
-    return true ;
-     }
-}
+    public boolean supprimerJeton() {
+        if (jetonCourant == null) {
+            System.out.print("Jeton innexistant ");
+            return false;
+        } else {
+            jetonCourant = null;
+            return true;
+        }
+    }
 
-//public boolean placerTrouNoir(){
-  //  trouNoir.placerTrouNoir(int, int);
-//}
+    public boolean placerTrouNoir() {
+        if (trouNoir == false) {
+            trouNoir = true;
+            System.out.println("Ajout de Trou Noir effectuÃ©");
+            return true;
+        } else {
+            System.out.println("Trou Noir deja present");
+            return false;
+        }
+    }
+
+    public boolean placerDesintegrateur(){
+     if (desintegrateur == false) {
+            desintegrateur = true;
+            System.out.println("Ajout du desintegrateur effectué");
+            return true;
+        } else {
+            System.out.println("desintegrateur deja present");
+            return false;
+        }
+       
+    } 
+public boolean recupererDesintegrateur (){
+ if (desintegrateur == true) {
+            desintegrateur = false;
+            System.out.println("Suppression du desintegrateur effectué");
+            return true;
+        } else {
+            System.out.println("pas de desintegrateur present");
+            return false;
+        }   
+}
+public boolean activerTrouNoir(){
+    if(trouNoir == true){
+        jetonCourant=null; // vide la cellule
+        trouNoir = false; // disparition du Trou Noir
+        System.out.println("Activation du Trou Noir");
+        return true;  
+    }
+    else{
+        System.out.println("Pas de Trou Noir dans cette cellule");
+        return false;
+    }
+}
 }
